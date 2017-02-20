a65a950f6999765ecbc5d2e1d7ec8ac5564e3794
luqiang21 <buaaluqiang@hotmail.com>
2017-02-19 20:32:14 -0700

    try other model

--- model-Nvidia.py
+++ model-Nvidia.py
@@ -86,40 +86,62 @@ from keras.layers import Dense, Activation, Convolution2D, Flatten, Lambda, Drop
 input_shape = (160, 320, 3)
 
 model = Sequential()
-model.add(Lambda(lambda x: x/127.5 - 1., input_shape=input_shape))
-model.add(Convolution2D(3, 1, 1, border_mode='same', name='color_conv'))
 
-model.add(Convolution2D(24, 5, 5, subsample=(2, 2), border_mode="valid"))
-model.add(Activation('relu'))
-
-model.add(Convolution2D(36, 5, 5, subsample=(2, 2), border_mode="valid"))
-model.add(Activation('relu'))
-
-model.add(Convolution2D(48, 3, 3, subsample=(1, 1), border_mode="valid"))
-model.add(Activation('relu'))
-
-model.add(Convolution2D(64, 3, 3, subsample=(1, 1), border_mode="valid"))
-model.add(Activation('relu'))
-
-model.add(Convolution2D(64, 3, 3, subsample=(1, 1), border_mode="valid"))
-model.add(Activation('relu'))
+model.add(Conv2D(24, 5, 5, border_mode='valid', subsample=(2,2), activation="relu", input_shape=(66, 200, 3)))
+model.add(BatchNormalization(axis=-1))
+model.add(Convolution2D(36, 5, 5, border_mode="valid", subsample=(2,2), activation="elu"))
+model.add(BatchNormalization(axis=-1))
+model.add(Convolution2D(48, 5, 5, border_mode="valid", subsample=(2,2), activation="elu"))
+model.add(BatchNormalization(axis=-1))
+model.add(Convolution2D(64, 3, 3, border_mode="valid", activation="elu"))
+model.add(BatchNormalization(axis=-1))
+model.add(Convolution2D(64, 3, 3, border_mode="valid", activation="elu"))
+model.add(BatchNormalization(axis=-1))
 
 model.add(Flatten())
-model.add(Dense(1164))
-model.add(Activation('relu'))
+model.add(BatchNormalization())
+model.add(Dense(100, activation="elu"))
+model.add(Dense(50, activation="elu"))
+model.add(Dense(10, activation="elu"))
+model.add(BatchNormalization())
+model.add(Dense(1, activation="linear"))
+model.summary()
 
-model.add(Dense(100))
-model.add(Activation('relu'))
-
-model.add(Dense(50))
-model.add(Activation('relu'))
-
-model.add(Dense(10))
-model.add(Activation('relu'))
-
-model.add(Activation('relu'))
-
-model.add(Dense(1))
+#
+# model.add(Lambda(lambda x: x/127.5 - 1., input_shape=input_shape))
+# model.add(Convolution2D(3, 1, 1, border_mode='same', name='color_conv'))
+#
+# model.add(Convolution2D(24, 5, 5, subsample=(2, 2), border_mode="valid"))
+# model.add(Activation('relu'))
+#
+# model.add(Convolution2D(36, 5, 5, subsample=(2, 2), border_mode="valid"))
+# model.add(Activation('relu'))
+#
+# model.add(Convolution2D(48, 3, 3, subsample=(1, 1), border_mode="valid"))
+# model.add(Activation('relu'))
+#
+# model.add(Convolution2D(64, 3, 3, subsample=(1, 1), border_mode="valid"))
+# model.add(Activation('relu'))
+#
+# model.add(Convolution2D(64, 3, 3, subsample=(1, 1), border_mode="valid"))
+# model.add(Activation('relu'))
+#
+# model.add(Flatten())
+# model.add(Dense(1164))
+# model.add(Activation('relu'))
+#
+# model.add(Dense(100))
+# model.add(Activation('relu'))
+#
+# model.add(Dense(50))
+# model.add(Activation('relu'))
+#
+# model.add(Dense(10))
+# model.add(Activation('relu'))
+#
+# model.add(Activation('relu'))
+#
+# model.add(Dense(1))
 model.compile(optimizer="adam", loss="mse")
 # model.fit(X_train, y_train, batch_size=100, nb_epoch=50,validation_data=(X_val, y_val), shuffle=True)
 model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch = 5, batch_size = 100)
--- model-new.py
+++ model-new.py
@@ -32,6 +32,16 @@ PATHS = [#'track1_central/driving_log.csv',
 			# 'track1_bridge/driving_log.csv'	,
 		 	# 'track1_bridge2/driving_log.csv',
 		 	'udacity/driving_log.csv' ]
+PATHS = ['track1_central/driving_log.csv',
+		   'track1_recovery/driving_log.csv',
+		   'track1_reverse/driving_log.csv',
+		   'track1_recovery_reverse/driving_log.csv',
+			'track1_test/driving_log.csv',
+
+			# 'track1_bridge/driving_log.csv'	,
+		 	# 'track1_bridge2/driving_log.csv',
+		 	# 'udacity/driving_log.csv'
+]
 # the first line is the column names.
 for PATH in PATHS:
 	with open(PATH) as csvfile:
