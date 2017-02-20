527f2c5e129bb80e8cee461914299d9ba5a6b0a1
luqiang21 <buaaluqiang@hotmail.com>
2017-02-19 17:36:52 -0700

    correct directory

--- model-Nvidia.py
+++ model-Nvidia.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 path = '../data_p/'
-path = 'C:/Users/Qiang/Downloads/data_p/'
+# path = 'C:/Users/Qiang/Downloads/data_p/'
 
 os.chdir(path)
 samples = []
